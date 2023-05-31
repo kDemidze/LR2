5587c5f89ce39017242d3672f837b418b47eb110
KDemidze <oskar2002diman@yandex.ru>
2023-05-31 20:39:02 +0300

    otchet

---
+++ create.bat
@@ -0,0 +1,23 @@
+echo off
+md kurov
+cd kurov
+md dmitry
+md alexandrovich
+pause
+echo off
+echo > 05032002.txt
+cd alexandrovich
+echo > huawei.txt
+cd ..
+cd ..
+pause
+echo off
+del kurov /S /Q /F
+pause
+echo off
+cd kurov
+rd dmitry
+rd alexandrovich
+cd .. 
+rd kurov
+pause
